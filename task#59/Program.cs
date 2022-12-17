d51f9ac7a0d19fcdb6563979d917c688261bfea3
Mikhail Starostin <starostin.mihail@mail.ru>
2022-12-17 17:05:45 +0300

    add task#59 my option

---
+++ task#59/Program.cs
@@ -0,0 +1,94 @@
+﻿/* Решение в группах задач:
+Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим 
+следующий массив:
+9 2 3
+4 2 4
+2 6 7 */
+Console.Clear();
+
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+
+PrintArrayWithoutRowAndColumnWithMinamalElement(array, GetMinValue(array).Item1, GetMinValue(array).Item2);
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+(int, int) GetMinValue(int[,] inputArray)
+{
+    int min = inputArray[0, 0];
+    int iMin = 0, jMin = 0;
+
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            if (inputArray[i, j] < min)
+            {
+                min = inputArray[i, j];
+                iMin = i;
+                jMin = j;
+            }
+        }
+    }
+    return (iMin, jMin);
+}
+
+void PrintArrayWithoutRowAndColumnWithMinamalElement(int[,] InputArray, int RowWithMinElement, int ColumnWithMinElement)
+{
+    for (int i = 0; i < InputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < InputArray.GetLength(1); j++)
+        {
+            if (i != RowWithMinElement && j != ColumnWithMinElement)
+            {
+                Console.Write(InputArray[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
