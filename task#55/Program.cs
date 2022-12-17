4fc9d66eea3a5ba4d63674dc783fc5c15c39df78
Mikhail Starostin <starostin.mihail@mail.ru>
2022-12-17 12:45:27 +0300

    add task#55 my option

---
+++ task#55/Program.cs
@@ -0,0 +1,74 @@
+﻿/* Решение задач в группах
+Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+Исходный массив:		Результат замены:
+9 2 3          			9 4 2			
+4 2 4          			2 2 6
+2 6 7 				    3 4 7 */
+Console.Clear();
+
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+Console.WriteLine();
+ReplaceRowsWithColumnsInArray(array);
+
+//Console.WriteLine($"Разница между максимальным и минимальным элементом в массиве = {GetMaxValue(array) - GetMinValue(array) }");
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplaceRowsWithColumnsInArray (int[,] inputArray)
+{
+    if (inputArray.GetLength(0)!=inputArray.GetLength(1))
+    {
+        Console.WriteLine("Замена невозможна, задайте массив с равным числом строк и столбцов.");
+    }
+    else
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+    {
+        for (int i = 0; i < inputArray.GetLength(0); i++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    }
+}
